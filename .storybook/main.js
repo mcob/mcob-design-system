686ad51405b0289e0dbce5ae5108268f188f6120
Minhee Myung <minnie.myung1001@gmail.com>
2023-03-18 22:01:28 -0400

    Add testing addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,17 @@
 module.exports = {
-  "stories": [
+  // Changes the load order of our stories. First loads the Intro page
+  // automatically import all files ending in *.stories.js|mdx
+  stories: [
+    "../src/Intro.stories.mdx",
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-a11y",
   ],
-  "framework": "@storybook/react"
-}
+  framework: "@storybook/react",
+};
